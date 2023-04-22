35c754b46ee61c27057379b1c1ee419bc08f845d
gizzSaw <gizzsaw@gmail.com>
2023-04-22 12:13:30 +0300

    add todo

--- js/todo.js
+++ js/todo.js
@@ -13,60 +13,66 @@ li.append(todoAddBtn)
 
 const getTodo = () => {
     const todoList = JSON.parse(localStorage.getItem('pomodoro') || '[]')
+    
+    return todoList
+}
 
-    if (!todoList.length) {
-        todoList.push({
-            id: 'default',
-            pomodoro: 0,
-            title: 'Помодоро',
-        })
+const addTodo = (title) => {
+    const todo = {
+        title,
+        pomodoro: 0,
+        id: Math.random().toString(16).substring(2, 8),
     }
 
-    return todoList
+    const todoList = getTodo()
+    todoList.push(todo)
+
+    localStorage.setItem('pomodoro', JSON.stringify(todoList))
+    return todo
+}
+
+const createTodoListItem = (todo) => {
+    if (todo.id !== 'default') {
+        const todoItem = document.createElement('li')
+        todoItem.classList.add('todo__item')
+
+        const todoItemWrapper = document.createElement('div')
+        todoItemWrapper.classList.add('todo__item-wrapper')
+        todoItem.append(todoItemWrapper)
+
+        const todoBtn = document.createElement('button')
+        todoBtn.classList.add('todo__btn')
+        todoBtn.textContent = todo.title
+
+        const editBtn = document.createElement('button')
+        editBtn.classList.add('todo__edit')
+        editBtn.ariaLabel = 'Редактировать задачу'
+
+        const delBtn = document.createElement('button')
+        delBtn.classList.add('todo__del')
+        delBtn.ariaLabel = 'Удалить задачу'
+
+        todoItemWrapper.append(todoBtn, editBtn, delBtn)
+
+        todoListElem.prepend(todoItem)
+
+        todoBtn.addEventListener('click', () => {
+            
+        })
+        editBtn.addEventListener('click', () => {
+            
+        })
+        delBtn.addEventListener('click', () => {
+            
+        })
+    }
 }
 
 const renderTodoList = (list) => {
     todoListElem.textContent = ''
-    
-    list.forEach((todo) => {
-        if (todo.id !== 'default') {
-            const todoItem = document.createElement('li')
-            todoItem.classList.add('todo__item')
-
-            const todoItemWrapper = document.createElement('div')
-            todoItemWrapper.classList.add('todo__item-wrapper')
-            todoItem.append(todoItemWrapper)
-
-            const todoBtn = document.createElement('button')
-            todoBtn.classList.add('todo__btn')
-            todoBtn.textContent = todo.title
-
-            const editBtn = document.createElement('button')
-            todoBtn.classList.add('todo__edit')
-            todoBtn.ariaLabel = 'Редактировать задачу'
-
-            const delBtn = document.createElement('button')
-            todoBtn.classList.add('todo__del')
-            todoBtn.ariaLabel = 'Удалить задачу'
-
-            todoItemWrapper.append(todoBtn, editBtn, delBtn)
-
-            todoListElem.prepend(todoItem)
-        }
-    })
-
+    list.forEach(createTodoListItem)
     todoListElem.append(li)
 }
-
-/* 
-        <li class="todo__item">
-          <div class="todo__item-wrapper">
-            <button class="todo__btn">Написать Pomodoro</button>
-            <button class="todo__edit" aria-label="Редактировать"></button>
-            <button class="todo__del" aria-label="Удалить"></button>
-          </div>
-        </li>
-*/
 
 const showTodo = () => {
     titleElem.textContent = state.activeTodo.title
@@ -75,8 +81,24 @@ const showTodo = () => {
 
 export const initTodo = () => {
     const todoList =  getTodo()
-    state.activeTodo = todoList[0]
+
+    if (!todoList.length) {
+        state.activeTodo = [{
+            id: 'default',
+            pomodoro: 0,
+            title: 'Помодоро',
+        }]
+    } else {
+        state.activeTodo = todoList[todoList.length - 1]
+    }
+   
     showTodo()
 
     renderTodoList(todoList)
+
+    todoAddBtn.addEventListener('click', () => {
+        const title = prompt('Введите имя задачи')
+        const todo = addTodo(title)
+        createTodoListItem(todo)
+    })
 }
